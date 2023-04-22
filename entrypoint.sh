c16591f0af3808bfb2a324a3ee929ec8eada80c7
deep-soft <deep-soft@users.noreply.github.com>
2023-04-22 12:55:04 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,13 @@ then
   cd $INPUT_DIRECTORY
 fi
 
-if [ "$INPUT_TYPE" = "zip" ] 
+if [ "$INPUT_TYPE" = "zip" ]
 then
   if [ "$RUNNER_OS" = "Windows" ]
   then
-    if [ -z "$INPUT_EXCLUSIONS" ] 
+    EXCLUSIONS=''
+    if [ -z "$INPUT_EXCLUSIONS" ]
     then
-      7z a -tzip $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-    else
-      EXCLUSIONS=''
-
       for EXCLUSION in $INPUT_EXCLUSIONS
       do
         EXCLUSIONS+=" -x!"
@@ -31,32 +28,32 @@ then
         EXCLUSIONS+=" -xr!"
         EXCLUSIONS+=$EXCLUSION
       done
-
-      7z a -tzip $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
     fi
+    7z a -tzip $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   else
-    if [ -z "$INPUT_EXCLUSIONS" ] 
+    EXCLUSIONS=""
+    if [ -z "$INPUT_EXCLUSIONS" ]
     then
-      zip -r $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-    else
-      zip -r $INPUT_FILENAME $INPUT_PATH -x $INPUT_EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+      EXCLUSIONS="-x $INPUT_EXCLUSIONS"
     fi
+    zip -r $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   fi
-elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] 
+elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] || [ "$INPUT_TYPE" = "tar.xz" ]
 then
-  if [ -z "$INPUT_EXCLUSIONS" ] 
+  EXCLUSIONS=''
+  if [ -z "$INPUT_EXCLUSIONS" ]
   then
-    tar -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-  else
-    EXCLUSIONS=''
-
     for EXCLUSION in $INPUT_EXCLUSIONS
     do
       EXCLUSIONS+=" --exclude="
       EXCLUSIONS+=$EXCLUSION
     done
-
-    tar $EXCLUSIONS -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+  fi
+  if [ "$INPUT_TYPE" == "tar.xz" ]
+  then
+    tar $EXCLUSIONS cvf - $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM | xz -9 > $INPUT_FILENAME.tar.xz || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+  else
+    tar $EXCLUSIONS -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }    
   fi
 else
   printf "\n⛔ Invalid archiving tool.\n"; exit 1;
