5fef96baa12720778a11434b4c27494095f89678
deep-soft <deep-soft@users.noreply.github.com>
2023-11-19 11:30:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,23 @@
 set -euf
 if [ "$DEBUG_MODE" = "yes" ]
 then
-  set -x
+  set -x;
 fi
 
-StartTime="$(date -u +%s)"
-CrtDate=$(date "+%F^%H:%M:%S")
-echo "Start: " $CrtDate
+StartTime="$(date -u +%s)";
+CrtDate=$(date "+%F^%H:%M:%S");
+echo "Start: " $CrtDate;
 
 # change path separator to /
-INPUT_DIRECTORY=$(echo $INPUT_DIRECTORY | tr '\\' /)
+INPUT_DIRECTORY=$(echo $INPUT_DIRECTORY | tr '\\' /);
+# change extension
+INPUT_FILENAME="${0%.*}".$INPUT_TYPE;
+
 printf "\n📦 Creating archive=[%s], dir=[%s], name=[%s], path=[%s], runner=[%s] ...\n" "$INPUT_TYPE" "$INPUT_DIRECTORY" "$INPUT_FILENAME" "$INPUT_PATH" "$RUNNER_OS"
 
 if [ "$INPUT_DIRECTORY" != "." ] 
 then
-  cd $INPUT_DIRECTORY
+  cd $INPUT_DIRECTORY;
   if [ "$DEBUG_MODE" = "yes" ]
   then
     echo "List dir:";
@@ -28,78 +31,78 @@ fi
 echo "List dir:";
 ls -l;
 
-ARCHIVE_SIZE=""
-INCLUSIONS="$INPUT_INCLUSIONS"
+ARCHIVE_SIZE="";
+INCLUSIONS="$INPUT_INCLUSIONS";
 
 if [ "$INPUT_TYPE" = "zip" ] || [ "$INPUT_TYPE" = "7z" ]
 then
   if [ "$RUNNER_OS" = "Windows" ]
   then
-    EXCLUSIONS=''
+    EXCLUSIONS='';
     if [ -n "$INPUT_EXCLUSIONS" ] || [ -n "$INPUT_RECURSIVE_EXCLUSIONS" ]
     then 
       for EXCLUSION in $INPUT_EXCLUSIONS
       do
-        EXCLUSIONS+=" -x!"
-        EXCLUSIONS+="$EXCLUSION"
+        EXCLUSIONS+=" -x!";
+        EXCLUSIONS+="$EXCLUSION";
       done
 
       for EXCLUSION in $INPUT_RECURSIVE_EXCLUSIONS
       do
-        EXCLUSIONS+=" -xr!"
-        EXCLUSIONS+="$EXCLUSION"
+        EXCLUSIONS+=" -xr!";
+        EXCLUSIONS+="$EXCLUSION";
       done
     fi
-    echo "CMD: 7z a -r -ssw -t$INPUT_TYPE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM"
-    7z a -r -ssw -t$INPUT_TYPE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-    echo 'Done'
-    ARCHIVE_SIZE=$(find . -name $INPUT_FILENAME -printf '(%s bytes) = (%k KB)')
+    echo "CMD: 7z a -r -ssw -t$INPUT_TYPE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM";
+    7z a -r -ssw -t$INPUT_TYPE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  };
+    echo 'Done';
+    ARCHIVE_SIZE=$(find . -name $INPUT_FILENAME -printf '(%s bytes) = (%k KB)');
   else
-    EXCLUSIONS=""
+    EXCLUSIONS="";
     if [ -n "$INPUT_EXCLUSIONS" ]
     then
-      EXCLUSIONS="-x $INPUT_EXCLUSIONS"
+      EXCLUSIONS="-x $INPUT_EXCLUSIONS";
     fi
-    echo "CMD: zip -r $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM"
-    zip -r $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-    echo 'Done'
+    echo "CMD: zip -r $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM";
+    zip -r $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  };
+    echo 'Done';
     if [ "$RUNNER_OS" != "macOS" ]
     then
-      ARCHIVE_SIZE=$(find . -name $INPUT_FILENAME -printf '(%s bytes) = (%k KB)')
+      ARCHIVE_SIZE=$(find . -name $INPUT_FILENAME -printf '(%s bytes) = (%k KB)');
     fi
   fi
 elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] || [ "$INPUT_TYPE" = "tar.xz" ]
 then
-  EXCLUSIONS='--exclude=*.tar* '
+  EXCLUSIONS='--exclude=*.tar* ';
   if [ -n "$INPUT_EXCLUSIONS" ]
   then
     for EXCLUSION in $INPUT_EXCLUSIONS
     do
-      EXCLUSIONS+=" --exclude="
-      EXCLUSIONS+="$EXCLUSION"
+      EXCLUSIONS+=" --exclude=";
+      EXCLUSIONS+="$EXCLUSION";
     done
   fi
   if [ "$INPUT_TYPE" == "tar.xz" ]
   then
-    tar $EXCLUSIONS -cv $INPUT_PATH $INCLUSIONS $INPUT_CUSTOM | xz -9 > $INPUT_FILENAME || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    tar $EXCLUSIONS -cv $INPUT_PATH $INCLUSIONS $INPUT_CUSTOM | xz -9 > $INPUT_FILENAME || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  };
   else
-    tar $EXCLUSIONS -zcvf $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }    
+    tar $EXCLUSIONS -zcvf $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  };
   fi
-  ARCHIVE_SIZE=$(find . -name $INPUT_FILENAME -printf '(%s bytes) = (%k KB)')
+  ARCHIVE_SIZE=$(find . -name $INPUT_FILENAME -printf '(%s bytes) = (%k KB)');
 else
   printf "\n⛔ Invalid archiving tool.\n"; exit 1;
 fi
 
-FinishTime="$(date -u +%s)"
-CrtDate=$(date "+%F^%H:%M:%S")
-echo "Finish: " $CrtDate
+FinishTime="$(date -u +%s)";
+CrtDate=$(date "+%F^%H:%M:%S");
+echo "Finish: " $CrtDate;
 
-ElapsedTime=$(( FinishTime - StartTime ))
-echo "Elapsed: $ElapsedTime"
+ElapsedTime=$(( FinishTime - StartTime ));
+echo "Elapsed: $ElapsedTime";
 
-printf "\n✔ Successfully created archive=[%s], dir=[%s], name=[%s], path=[%s], size=[%s], runner=[%s] duration=[%ssec]...\n" "$INPUT_TYPE" "$INPUT_DIRECTORY" "$INPUT_FILENAME" "$INPUT_PATH" "$ARCHIVE_SIZE" "$RUNNER_OS" "$ElapsedTime"
+printf "\n✔ Successfully created archive=[%s], dir=[%s], name=[%s], path=[%s], size=[%s], runner=[%s] duration=[%ssec]...\n" "$INPUT_TYPE" "$INPUT_DIRECTORY" "$INPUT_FILENAME" "$INPUT_PATH" "$ARCHIVE_SIZE" "$RUNNER_OS" "$ElapsedTime";
 if [[ $INPUT_FILENAME =~ ^/ ]]; then
-  echo "$INPUT_ZIP_RELEASE_ARCHIVE=$INPUT_FILENAME" >> $GITHUB_ENV
+  echo "$INPUT_ZIP_RELEASE_ARCHIVE=$INPUT_FILENAME" >> $GITHUB_ENV;
 else
-  echo "$INPUT_ZIP_RELEASE_ARCHIVE=$INPUT_DIRECTORY/$INPUT_FILENAME" >> $GITHUB_ENV
+  echo "$INPUT_ZIP_RELEASE_ARCHIVE=$INPUT_DIRECTORY/$INPUT_FILENAME" >> $GITHUB_ENV;
 fi
