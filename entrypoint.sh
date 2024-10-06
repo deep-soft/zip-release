cd942d01240c092e82c043656b3e82ca118a20c1
deep-soft <deep-soft@users.noreply.github.com>
2024-10-06 18:09:36 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,8 +66,8 @@ if [[ "$INPUT_TYPE" == "zip" ]] || [[ "$INPUT_TYPE" == "7z" ]]; then
     if [[ $INPUT_VOLUME_SIZE != '' ]]; then
       VOL_SIZE="-v$INPUT_VOLUME_SIZE";
     fi
-    echo "CMD:[7z a -r -ssw -t$INPUT_TYPE $VOL_SIZE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM]";
-    7z a -r -ssw -t$INPUT_TYPE $VOL_SIZE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  };
+    echo "CMD:[7z a -r -ssw -snh -snl -t$INPUT_TYPE $VOL_SIZE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM]";
+    7z a -r -ssw -snh -snl -t$INPUT_TYPE $VOL_SIZE $INPUT_FILENAME $INPUT_PATH $INCLUSIONS $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  };
     echo 'Done';
     #ARCHIVE_SIZE=$(find . -name "$INPUT_FILENAME*" -printf '(%s bytes) = (%k KB)\p');
     #ls -la "$INPUT_FILENAME*" || true;
